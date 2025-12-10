501062d1ba4355bba3f671bcdede59b1275aba39
AMGHAR-ELMAHDI <amgharmahdi1@gmail.com>
2025-12-10 18:29:09 +0100

    fixed meta pixel tracking on contact form

--- next.config.js
+++ next.config.js
@@ -65,11 +65,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googletagmanager.com https://*.google-analytics.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googletagmanager.com https://*.google-analytics.com https://connect.facebook.net;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' data: https://*.google-analytics.com https://*.googletagmanager.com ${NEXT_PUBLIC_SERVER_URL};
+              img-src 'self' data: https://*.google-analytics.com https://*.googletagmanager.com https://www.facebook.com ${NEXT_PUBLIC_SERVER_URL};
               font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
+              connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://www.facebook.com;
               frame-src 'self';
               object-src 'none';
               base-uri 'self';
